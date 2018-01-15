2bc1709e26664e08fca6bd8a168df81424f52602
Artyom Losev <900000miles@gmail.com>
2018-01-15 21:38:01 +0500

    additional data

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import requests
-from lxml import html
+import json
 import csv
+from lxml import html
 
 URL = 'https://icosource.io'
 
@@ -25,65 +26,115 @@ def check_location(page_obj):
         return False
     return True
 
-def get_full_data():
-    s = requests.session()
+def get_json_data(data):
+    data = data.content
+    data = data.decode('utf-8')
+    data = json.loads(data)
+    return data
 
-    data = {
-        'action': 'ico_show_more',
-        'ico_paged': 5
-    }
-    d = s.post('https://icosource.io/wp-admin/admin-ajax.php', data)
-    data = html.fromstring(d.content)
-    print('*' * 80)
-    print(data[0].attrib)
+def get_additional_data():
+    res_array = []
+    ready = False
+    index = 2
 
-    d = s.get(URL)
-    return d
+    while (not ready):
+        data = {
+            'action': 'ico_show_more',
+            'ico_paged': index
+        }
+        index += 1
+
+        d = requests.post('https://icosource.io/wp-admin/admin-ajax.php', data)
+        data = get_json_data(d)
+        # data = d.content
+        # data = data.decode('utf-8')
+        # data = json.loads(data)
+
+        current = html.fromstring(data['html']['current'])
+        current = current.xpath('//*[@class="lp-grid-box-description "]')
+        if current:
+            current = prepare_additional_data(current)
+
+        upcoming = html.fromstring(data['html']['upcoming'])
+        upcoming = upcoming.xpath('//*[@class="lp-grid-box-description "]')
+        if upcoming:
+            upcoming = prepare_additional_data(upcoming)
+
+        res_array += current
+        res_array += upcoming
+
+        if (not current) and (not upcoming):
+            ready = True
+
+    return res_array
+
+
+def prepare_additional_data(page):
+    res_array = []
+    for el in page:
+        source = el[0][0][0].attrib['href']
+        date = el[0][1][0].text
+        data_row = get_ico_page_data(source)
+        data_row['date'] = date
+        data_row['url_source'] = source
+
+        res_array.append(data_row)
+    return res_array  
+
+def get_ico_page_data(url_source):
+    # ico page data
+    ico_page = requests.get(url_source)
+    ico_data = html.fromstring(ico_page.content)
+
+    name = ico_data.xpath('//*[@id="details"]/div/h3/text()')[0]
+    print('processing ', name)
+
+    descr = ico_data.xpath('//*[@id="details"]/div/p[1]/text()')[0]
+
+    dev_xpath ='//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div[2]/ul/li[4]/a'
+    developer = get_link(ico_data, dev_xpath)
+
+    site_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/a'
+        if check_location(ico_data) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[1]/a')
+    site = get_link(ico_data, site_xpath)
+
+    whitepaper_link_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[3]/a' 
+        if check_location(ico_data) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[2]/a')
+    whitepaper_link = get_link(ico_data, whitepaper_link_xpath)
+
+    return {
+        'name': name,
+        'descr': descr,
+        'developer':developer,
+        'site': site
+    } 
+
 
 if __name__ == '__main__':
-    get_full_data()
-
     result_array = []
-
     res = requests.get(URL)
-    # res = get_full_data()
     data = html.fromstring(res.content)
 
     container_elements = data.xpath('//*[@class="col-md-12 lp-grid-box-contianer list_view card1 lp-grid-box-contianer1 "]')
-    print(container_elements)
+
     for el in container_elements:
         # main page data
         url_source = el.attrib['data-posturl']
         name = el[0][1][0][0][0].text
         date = el[0][1][0][1][0].text
 
-        # ico_page data
-        ico_page = requests.get(url_source)
-        ico_data = html.fromstring(ico_page.content)
+        data_row = get_ico_page_data(url_source)
 
-        descr = ico_data.xpath('//*[@id="details"]/div/p[1]/text()')[0]
+        data_row['date'] = date
+        data_row['url_source'] = url_source
 
-        dev_xpath ='//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div[2]/ul/li[4]/a'
-        developer = get_link(ico_data, dev_xpath)
-
-        site_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/a'
-            if check_location(ico_data) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[1]/a')
-        site = get_link(ico_data, site_xpath)
-
-        whitepaper_link_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[3]/a' 
-            if check_location(ico_data) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[2]/a')
-        whitepaper_link = get_link(ico_data, whitepaper_link_xpath)
-
-        data_row = {
-            'url_source': url_source,
-            'name': name,
-            'date': date,
-            'descr': descr,
-            'developer': developer,
-            'site': site,
-            'whitepaper_link': whitepaper_link
-        }
         result_array.append(data_row)
-        print(data_row)
 
+    print('*' * 80)
+    print('Processing additional data.')
+
+    additional_data = get_additional_data()
+    
+    result_array += additional_data
+    print(len(result_array))
     save_to_csv(result_array)
