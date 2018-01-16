2720c8b18e417fdb6d655902490f34846a303cb6
Artyom Losev <900000miles@gmail.com>
2018-01-16 17:17:41 +0500

    ref

--- cb_scrapper.py
+++ cb_scrapper.py
@@ -8,6 +8,18 @@ FILENAME = 'cb_ico_table.csv'
 URL = 'https://icosource.io'
 PAGINATOR_URL = 'https://icosource.io/wp-admin/admin-ajax.php'
 
+xpath_expressions = {
+    'main_page_container_element': '//*[@class="col-md-12 lp-grid-box-contianer list_view card1 lp-grid-box-contianer1 "]',
+    'location_img': '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[1]/a/span[1]/img',
+    'ico_page_name': '//*[@id="details"]/div/h3/text()',
+    'ico_page_description': '//*[@id="details"]/div/p[1]/text()',
+    'ico_page_developer': '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div[2]/ul/li[4]/a',
+    'ico_page_site': '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/a',
+    'ico_page_site_secondary': '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[1]/a',
+    'ico_page_whitepaper_link': '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[3]/a',
+    'ico_page_whitepaper_link_secondary':'//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[2]/a'
+}
+
 class Scrapper:
     def __init__(self):
         self.data = []
@@ -40,9 +52,8 @@ class Scrapper:
     def parse_main_page_ico_data(self, data):
         data = self.convert_to_lxml_tree(data)
         result = []
-        container_elements = data.xpath('//*[@class="col-md-12 lp-grid-box-contianer list_view card1 lp-grid-box-contianer1 "]')
+        container_elements = data.xpath(xpath_expressions['main_page_container_element'])
         for el in container_elements:
-            # main page data
             url_source = el.attrib['data-posturl']
             date = el.find('.//li[@class="middle"]').text
             result.append({
@@ -52,7 +63,7 @@ class Scrapper:
         return result
 
     def location_exists(self, page_obj):
-        loc = page_obj.xpath('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[1]/a/span[1]/img')
+        loc = page_obj.xpath(xpath_expressions['location_img'])
         if not loc:
             return False
         return True
@@ -66,20 +77,18 @@ class Scrapper:
     def parse_ico_page_data(self, page):
         page = self.convert_to_lxml_tree(page)
 
-        name = page.xpath('//*[@id="details"]/div/h3/text()')[0]
+        name = page.xpath(xpath_expressions['ico_page_name'])[0]
         print('processing ', name)
 
-        descr = page.xpath('//*[@id="details"]/div/p[1]/text()')[0]
+        descr = page.xpath(xpath_expressions['ico_page_description'])[0]
+        developer = self.get_link(page, xpath_expressions['ico_page_developer'])
 
-        dev_xpath ='//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div[2]/ul/li[4]/a'
-        developer = self.get_link(page, dev_xpath)
-
-        site_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/a'
-            if self.location_exists(page) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[1]/a')
+        site_xpath = (xpath_expressions['ico_page_site'] if self.location_exists(page) 
+            else xpath_expressions['ico_page_site_secondary'])
         site = self.get_link(page, site_xpath)
 
-        whitepaper_link_xpath = ('//*[@id="page"]/section/div[2]/div/div/div[2]/div/div[1]/div[2]/ul/li[3]/a' 
-            if self.location_exists(page) else '//*[@id="page"]/section/div[2]/div/div/div[2]/div/div/div/ul/li[2]/a')
+        whitepaper_link_xpath = (xpath_expressions['ico_page_whitepaper_link'] 
+            if self.location_exists(page) else xpath_expressions['ico_page_whitepaper_link_secondary'])
         whitepaper_link = self.get_link(page, whitepaper_link_xpath)
 
         return {
@@ -118,7 +127,6 @@ class Scrapper:
 
         return res_array
 
-
     def get_paginated_data(self):
         res_array = []
         ready = False
@@ -137,33 +145,30 @@ class Scrapper:
             source = el.find('.//h4/a').attrib['href']
             date = el.find('.//li[@class="middle"]').text
 
-            # data_row = get_ico_page_data(source)
-            # data_row['date'] = date
-            # data_row['url_source'] = source
-
             res_array.append({
                 'url_source': source,
                 'date': re.sub(r"[\n\t\r]*", "", date)
             })
         return res_array  
 
-    def start(self):
+    def get_data(self):
         main_page_data = self.fetch_content(URL)
         res = self.parse_main_page_ico_data(main_page_data)
 
         for i in res:
             self.get_ico_page_data(i)
-        print(res)
 
         paginated_data = self.get_paginated_data()
         res2 = self.parse_paginated_page_ico_data(paginated_data)
         for i in res2:
             self.get_ico_page_data(i)
 
-        print(res2)
         self.data = res + res2
+
+    def save_data_to_file(self):
         self.save_to_csv(self.data, FILENAME)
 
 if __name__ == '__main__':
     scrapper = Scrapper()
-    scrapper.start()
+    scrapper.get_data()
+    scrapper.save_data_to_file()
